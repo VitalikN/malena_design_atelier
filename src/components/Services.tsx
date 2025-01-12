5d1106b5f5d956e9b30356c58922d50f45c02e5c
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2025-01-12 21:34:40 +0200

    fix mx-auto

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -12,27 +12,27 @@ const Services = () => {
           Наші послуги:
         </h2>
         <ul className="grid sm:grid-cols-2 xl:grid-cols-4 gap-4">
-          <li className="p-2  ">
+          <li className="p-2 max-w-72  mx-auto">
             <ImScissors className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Індивідуальний пошив — створення суконь та іншого одягу під ваше
               замовлення з урахуванням побажань і стилю.
             </p>
           </li>
-          <li>
+          <li className="p-2 max-w-72  mx-auto">
             <LiaCheckCircle className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Контроль якості — ретельна перевірка кожної деталі для ідеального
               результату.
             </p>
           </li>
-          <li>
+          <li className="p-2 max-w-72  mx-auto">
             <TbIroningSteam className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Відпарювання — професійна підготовка суконь до важливого дня.
             </p>
           </li>
-          <li>
+          <li className="p-2 max-w-72  mx-auto">
             <FaRegUser className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Консультація стиліста салону — допомога у виборі фасону та стилю,
@@ -40,21 +40,21 @@ const Services = () => {
             </p>
           </li>
         </ul>
-        <ul className="grid sm:grid-cols-2 xl:grid-cols-3 gap-4">
-          <li>
+        <ul className="grid sm:grid-cols-2 xl:grid-cols-3 gap-4  ">
+          <li className="p-2 max-w-72  mx-auto">
             <BiCloset className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Зберігання сукні до весілля — безпечне зберігання вашого вбрання у
               нашому салоні.
             </p>
           </li>
-          <li>
+          <li className="p-2 max-w-72 mx-auto ">
             <GiSewingMachine className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Ательє — професійний пошив та корекція готових суконь.
             </p>
           </li>
-          <li>
+          <li className="p-2 max-w-72 mx-auto ">
             <GiSewingNeedle className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
               Пошив усіх видів одягу на замовлення — від елегантних суконь до
