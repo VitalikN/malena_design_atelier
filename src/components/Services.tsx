7ee28f0444ae95c3785ae9080cc6c147d24f1c42
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2025-01-13 11:48:35 +0200

    Services add  order GiAmpleDress

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,7 @@
 import { FaRegUser } from "react-icons/fa";
 import { TbIroningSteam } from "react-icons/tb";
 import { ImScissors } from "react-icons/im";
-import { GiSewingNeedle, GiSewingMachine } from "react-icons/gi";
+import { GiSewingNeedle, GiSewingMachine, GiAmpleDress } from "react-icons/gi";
 import { BiCloset } from "react-icons/bi";
 import { LiaCheckCircle } from "react-icons/lia";
 const Services = () => {
@@ -15,50 +15,56 @@ const Services = () => {
           <li className="p-2 max-w-72  mx-auto">
             <ImScissors className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Індивідуальний пошив — створення суконь та іншого одягу під ваше
-              замовлення з урахуванням побажань і стилю.
+              Індивідуальний пошив: створюємо сукні за вашим дизайном та
+              побажаннями
             </p>
           </li>
           <li className="p-2 max-w-72  mx-auto">
             <LiaCheckCircle className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Контроль якості — ретельна перевірка кожної деталі для ідеального
-              результату.
+              Контроль якості: кожна деталь перевіряється для ідеального
+              вигляду.
             </p>
           </li>
           <li className="p-2 max-w-72  mx-auto">
             <TbIroningSteam className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Відпарювання — професійна підготовка суконь до важливого дня.
+              Відпарювання: професійна підготовка суконь для важливих подій.
             </p>
           </li>
           <li className="p-2 max-w-72  mx-auto">
             <FaRegUser className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Консультація стиліста салону — допомога у виборі фасону та стилю,
-              що підкреслить вашу унікальність.
+              Консультація стиліста: підбір фасону, що відображає вашу
+              індивідуальність.
             </p>
           </li>
-        </ul>
-        <ul className="grid sm:grid-cols-2 xl:grid-cols-3 gap-4  ">
           <li className="p-2 max-w-72  mx-auto">
             <BiCloset className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Зберігання сукні до весілля — безпечне зберігання вашого вбрання у
-              нашому салоні.
+              Зберігання до весілля: безпечне зберігання вашого образу у нашому
+              салоні.
             </p>
           </li>
           <li className="p-2 max-w-72 mx-auto ">
             <GiSewingMachine className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Ательє — професійний пошив та корекція готових суконь.
+              Корекція готових суконь: професійна підгонка для ідеальної
+              посадки.
             </p>
           </li>
           <li className="p-2 max-w-72 mx-auto ">
             <GiSewingNeedle className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
             <p className="text-center">
-              Пошив усіх видів одягу на замовлення — від елегантних суконь до
-              повсякденних речей.
+              Пошив повсякденного одягу: стильні речі для будь-яких випадків
+              життя.
+            </p>
+          </li>
+          <li className="p-2 max-w-72 mx-auto ">
+            <GiAmpleDress className=" m-2 xs:size-10 md:size-12 text-gradientTo mx-auto " />
+            <p className="text-center">
+              Прокат вечірніх суконь: великий вибір елегантних суконь для
+              особливих моментів.
             </p>
           </li>
         </ul>
